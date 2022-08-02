3b3b765b4b9566d801aa3e136f22ede30f513a7a
Steven Bleau <bleausd@gmail.com>
2022-08-02 15:09:18 -0500

    showHistory function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,33 +11,50 @@ function readyNow(){
     //clear button click event
     $('body').on('click', '#cBtn', clearInputFields);
 }
-//VARIABLES
-let operator;
 
+
+
+
+
+//VARIABLES
+    let operator;
+    let history;
+    let calculation = {
+        inputOne: 0,
+        inputTwo: 0,
+        operator: '+',
+        answer: 0
+}
 
 
 //LOGIC:
 
-//STORE OPERATOR USED FOR CALCULATION
-function setOperator(){
-    console.log('in "setOperator"');
-    operator = $(this).attr('data-operator');
-        console.log('the operator selected is ', operator);
-}
-//CLEAR INPUT FIELDS
-function clearInputFields(){
-    console.log('in "clearInputFields"');
-    $('.inputFields').val('');
-}
-//APPEND CALCULATION HISTORY TO DOM
-function showHistory(){
-    console.log('in "showHistory');
-}
+    //STORE OPERATOR USED FOR CALCULATION
+    function setOperator(){
+        console.log('in "setOperator"');
+        operator = $(this).attr('data-operator');
+            console.log('the operator selected is ', operator);
+    }
+    //CLEAR INPUT FIELDS
+    function clearInputFields(){
+        console.log('in "clearInputFields"');
+        $('.inputFields').val('');
+    }
+    //APPEND CALCULATION HISTORY TO DOM
+    function showHistory(){
+        console.log('in "showHistory');
+        $('#historyList').empty();
+        $('#historyList').append(`
+            <li>${calculation.inputOne} ${calculation.operator} ${calculation.inputTwo} = ${calculation.answer}</li>
+        `);
+    }
 
-//CREATE OBJECT WITH INPUT, OPERATOR, AND TOTAL PROPERTIES; CALL POST; CLEAR INPUTS; DISPLAY HISTORY
-function submit(){
-    console.log('in "submit"');
-}
+
+    //CREATE OBJECT WITH INPUT, OPERATOR, AND TOTAL PROPERTIES; CALL POST; CLEAR INPUTS; DISPLAY HISTORY
+    function submit(){
+        console.log('in "submit"');
+        showHistory();
+    }
 
 
 //AJAX OPERATIONS:
